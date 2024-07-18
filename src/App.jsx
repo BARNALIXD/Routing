57d4fca0a4ff487ab913c3827094bc10d732c82f
user.name.like : "barnalixd <user.email.like : dasbarnali690@gcekbpatna.ac.in>
2024-07-18 11:58:35 +0530

    links and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import User from './components/User';
 
-function App() {
+const App = ()=> {
   return (
-    <div>App</div>
-  )
-}
+    <>
+    <nav className='mt-10 flex justify-center'>
+      <Link to="/">Home</Link>
+      <Link to="/user">User</Link>
+      <Link to="/about">About</Link>
 
+    </nav>
+
+
+   <Routes>
+      <Route path='/' element ={<Home/>}/>
+      <Route path='/user' element ={<User/>}/>
+      <Route path='/about' element ={<About/>}/>
+
+   </Routes>
+
+    </>
+  );
+};
 export default App
